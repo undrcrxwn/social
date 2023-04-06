bdbdda8c8d9822311b73b811bca07aa56800bafe
zendet <hozztie@gmail.com>
2023-04-07 01:27:09 +0700

    naming fixed

--- src/CrowdParlay.Social.Domain/Entities/Post.cs
+++ src/CrowdParlay.Social.Domain/Entities/Post.cs
@@ -3,7 +3,7 @@ namespace CrowdParlay.Social.Domain.Entities;
 public class Post
 {
     public int Id { get; set; }
-    public Discussion DiscussionId { get; set; } = default!;
+    public Discussion Discussion { get; set; } = default!;
     public string Content { get; set; } = default!;
     public Guid CreatedBy { get; set; }
     public DateTime CreatedAt { get; set; }
